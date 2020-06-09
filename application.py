bd1f7d7d64f3f9cdb54f10986030c662c5e8882d
Zach Bodi <zachbodi@bu.edu>
2020-06-08 22:09:32 -0400

    Requests ingredients from API and checks MySQL Database for matches

--- application.py
+++ application.py
@@ -1,16 +1,58 @@
 from flask import Flask
+from key import KEY
+import mysql.connector
+import requests
+import json
 
 application = Flask(__name__)
 
+mydb = mysql.connector.connect(
+	host="wcppdatabase.ckosxabna9k7.us-east-2.rds.amazonaws.com",
+	user="admin",
+	passwd="Qf4Tzvi&7J##UxUgjDVz",
+	database="appData"
+)
+
 @application.route("/")
 def home():
-    return "Hello, World!"
+	return "Hello, World!"
 
 @application.route('/', defaults={'path': ''})
 @application.route('/<path:path>')
 def catch_all(path):
-    barcode = path
-    return barcode
+	url = "https://api.edamam.com/api/food-database/parser?app_id=bb34f199&app_key={}&upc={}".format(KEY, path) 
+	response = requests.get(url)
+	product = response.json()
+    
+	try:
+		ingredients = product["hints"][0]["food"]["foodContentsLabel"]
+		ingredients = ingredients.lower()
+		ingredients = ingredients[:ingredients.index('.')]
+		ingredients = ingredients.replace('[', ';')
+		ingredients = ingredients.replace('.', '')
+		ingredients = ingredients.replace(']', '')
+		ingredients = ingredients.replace(' (',', ')
+		ingredients = ingredients.replace('), ', ', ')
+		ingredients = ingredients.replace('(','')
+		ingredients = ingredients.replace(')','')
+		ingredients = ingredients.replace('*','')
+		ingredients = ingredients.replace(' and ',', ')
+		ingredients = ingredients.replace(",", ";")
+		ingredients = ingredients.split(";")
+		ingredients = [x.strip() for x in ingredients]
+
+		outputResponse = {"tested": []}
+		mycursor = mydb.cursor()
+		for ingredient in ingredients:
+			mycursor.execute("SELECT light FROM chemicals WHERE name LIKE '%{}%'")
+			sqlResponse = mycursor.fetchall()
+			for row in sqlResponse:
+				outputResponse.tested.append((ingredient, str(row)))
+		return str(outputResponse)
+	except:
+		if "error" in product:
+			return "null"
+		raise Exception("something went wrong in processing")
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0',port=8080,debug=True)
+    application.run(host='localhost',port=8080,debug=True)
